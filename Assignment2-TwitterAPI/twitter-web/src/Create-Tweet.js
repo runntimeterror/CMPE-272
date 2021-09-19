64f0c599f57faeccdbbeb19113611386d8fb13b8
Soham Bhattacharjee <soham.onlyn@gmail.com>
2021-09-18 19:09:54 -0700

    checkpoint

--- Assignment2-TwitterAPI/twitter-web/src/Create-Tweet.js
+++ Assignment2-TwitterAPI/twitter-web/src/Create-Tweet.js
@@ -2,7 +2,13 @@ import './Create-Tweet.css'
 
 function CreateTweet() {
     return <div className="create-tweet-wrapper">
-        Create Tweet
+        <div className="user-icon">
+            <img height="50" width="50" src="/user-icon.png"></img>
+        </div>
+        <div className="tweet-text">
+            <div contentEditable="true">What's happening?</div>
+            <div className="tweet-button">Tweet</div>
+        </div>
     </div>
 }
 
