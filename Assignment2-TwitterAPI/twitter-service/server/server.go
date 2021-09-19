c24efcabe7942e2ea8d3c143420e61f920521dd2
Rajat Banerjee <rajatmig29@gmail.com>
2021-09-19 00:14:32 -0700

    adding content type

--- Assignment2-TwitterAPI/twitter-service/server/server.go
+++ Assignment2-TwitterAPI/twitter-service/server/server.go
@@ -36,6 +36,84 @@ type config struct {
 	deleteTweetPath string
 }
 
+type Tweet struct {
+	CreatedAt string `json:"created_at"`
+	ID        int64  `json:"id"`
+	IDStr     string `json:"id_str"`
+	Text      string `json:"text"`
+	Truncated bool   `json:"truncated"`
+	Entities  struct {
+		Hashtags     []interface{} `json:"hashtags"`
+		Symbols      []interface{} `json:"symbols"`
+		UserMentions []interface{} `json:"user_mentions"`
+		Urls         []interface{} `json:"urls"`
+	} `json:"entities"`
+	Source               string      `json:"source"`
+	InReplyToStatusID    interface{} `json:"in_reply_to_status_id"`
+	InReplyToStatusIDStr interface{} `json:"in_reply_to_status_id_str"`
+	InReplyToUserID      interface{} `json:"in_reply_to_user_id"`
+	InReplyToUserIDStr   interface{} `json:"in_reply_to_user_id_str"`
+	InReplyToScreenName  interface{} `json:"in_reply_to_screen_name"`
+	User                 struct {
+		ID          int64       `json:"id"`
+		IDStr       string      `json:"id_str"`
+		Name        string      `json:"name"`
+		ScreenName  string      `json:"screen_name"`
+		Location    string      `json:"location"`
+		Description string      `json:"description"`
+		URL         interface{} `json:"url"`
+		Entities    struct {
+			Description struct {
+				Urls []interface{} `json:"urls"`
+			} `json:"description"`
+		} `json:"entities"`
+		Protected                      bool          `json:"protected"`
+		FollowersCount                 int           `json:"followers_count"`
+		FriendsCount                   int           `json:"friends_count"`
+		ListedCount                    int           `json:"listed_count"`
+		CreatedAt                      string        `json:"created_at"`
+		FavouritesCount                int           `json:"favourites_count"`
+		UtcOffset                      interface{}   `json:"utc_offset"`
+		TimeZone                       interface{}   `json:"time_zone"`
+		GeoEnabled                     bool          `json:"geo_enabled"`
+		Verified                       bool          `json:"verified"`
+		StatusesCount                  int           `json:"statuses_count"`
+		Lang                           interface{}   `json:"lang"`
+		ContributorsEnabled            bool          `json:"contributors_enabled"`
+		IsTranslator                   bool          `json:"is_translator"`
+		IsTranslationEnabled           bool          `json:"is_translation_enabled"`
+		ProfileBackgroundColor         string        `json:"profile_background_color"`
+		ProfileBackgroundImageURL      interface{}   `json:"profile_background_image_url"`
+		ProfileBackgroundImageURLHTTPS interface{}   `json:"profile_background_image_url_https"`
+		ProfileBackgroundTile          bool          `json:"profile_background_tile"`
+		ProfileImageURL                string        `json:"profile_image_url"`
+		ProfileImageURLHTTPS           string        `json:"profile_image_url_https"`
+		ProfileLinkColor               string        `json:"profile_link_color"`
+		ProfileSidebarBorderColor      string        `json:"profile_sidebar_border_color"`
+		ProfileSidebarFillColor        string        `json:"profile_sidebar_fill_color"`
+		ProfileTextColor               string        `json:"profile_text_color"`
+		ProfileUseBackgroundImage      bool          `json:"profile_use_background_image"`
+		HasExtendedProfile             bool          `json:"has_extended_profile"`
+		DefaultProfile                 bool          `json:"default_profile"`
+		DefaultProfileImage            bool          `json:"default_profile_image"`
+		Following                      bool          `json:"following"`
+		FollowRequestSent              bool          `json:"follow_request_sent"`
+		Notifications                  bool          `json:"notifications"`
+		TranslatorType                 string        `json:"translator_type"`
+		WithheldInCountries            []interface{} `json:"withheld_in_countries"`
+	} `json:"user"`
+	Geo           interface{} `json:"geo"`
+	Coordinates   interface{} `json:"coordinates"`
+	Place         interface{} `json:"place"`
+	Contributors  interface{} `json:"contributors"`
+	IsQuoteStatus bool        `json:"is_quote_status"`
+	RetweetCount  int         `json:"retweet_count"`
+	FavoriteCount int         `json:"favorite_count"`
+	Favorited     bool        `json:"favorited"`
+	Retweeted     bool        `json:"retweeted"`
+	Lang          string      `json:"lang"`
+}
+
 func Init() *Server {
 	r := mux.NewRouter()
 	secretKeys := &secretKeys{
@@ -104,6 +182,7 @@ func (s *Server) getAllTweets(w http.ResponseWriter, r *http.Request) {
 
 	defer resp.Body.Close()
 	body, _ := ioutil.ReadAll(resp.Body)
+	w.Header().Add("content-type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(body)
 }
@@ -132,7 +211,8 @@ func (s *Server) createTweet(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(string(body))
+
+	w.Header().Add("content-type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(body))
 
@@ -160,7 +240,8 @@ func (s *Server) deleteTweet(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(string(body))
+
+	w.Header().Add("content-type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(body))
 }
